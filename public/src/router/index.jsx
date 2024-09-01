c6653f38f0ddc28dcb78f71c64efed83fde6c234
RizkyHidayat22 <rizkyhidayat0422@gmail.com>
2024-09-01 18:09:08 +0700

    fix router

--- public/src/router/index.jsx
+++ public/src/router/index.jsx
@@ -1,17 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import DetaiProduct from "../view/DetailProduct";
-import Homepage from "../view/homepage";
+import Homepage from "../view/HomePage";
 
-const router =  createBrowserRouter([
-    {
-        path : "/",
-        element : <Homepage/>
-    },
-    {
-        path : "/detail/:id",
-        element : <DetaiProduct/>
-    },
-])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/detail/:id",
+    element: <DetaiProduct />,
+  },
+]);
 
-export default router
+export default router;
